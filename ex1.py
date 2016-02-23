423dc61b850856914a07f32edc4681b33560519d
Markus A. Roth <maroth@gmail.com>
2016-02-23 12:40:54 +0100

    display training set correctly

--- ex1.py
+++ ex1.py
@@ -1,5 +1,22 @@
 import csv
+import matplotlib.pyplot as plot
+import numpy as np
+
+size = 28
+
 with open('train.csv', 'rb') as train_file:
 	train_data = csv.reader(train_file, delimiter=',')
-	for row in train_data:
-		print ", ".join(row)
+	for data_item in train_data:
+		data_item_bitmap = [[0 for x in range(size)] for x in range(size)]
+		data_item_index = 0
+		for row_index in range(size):
+			for col_index in range(size):
+				data_item_bitmap[row_index][col_index] = float(data_item[data_item_index + 1])
+				data_item_index += 1
+				
+		print(data_item[0])
+		plot.imshow(data_item_bitmap)
+		plot.gray()
+		plot.show()
+
+
