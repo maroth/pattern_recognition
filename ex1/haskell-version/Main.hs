c1542a1ef8843fd38350503d4675fbd7efac0777
Markus Roth <maroth@gmail.com>
2016-03-11 19:12:11 +0100

    working :)

--- ex1/haskell-version/Main.hs
+++ ex1/haskell-version/Main.hs
@@ -29,20 +29,35 @@ vote tieBreaker neighbors = breakTie $ takeWhileSameLength $ sortByLength $ grou
     breakTie a = if length a == 1 then value $ head $ head a else value $ minimumBy (comparing tieBreaker) (concat a)
 
 toLetter :: [String] -> Letter
-toLetter [] = error "empty letter"
+toLetter [] = error "empty list"
 toLetter (l:f) = Letter (read l :: Int) (map (\x -> read x :: Float) f)
 
 main :: IO ()
 main = do
-    trainFile <- readFile "../train_small.csv"
-    let csvTrainingSet = parseCsv trainFile where
+    trainFile <- readFile "../train.csv"
+    testFile <- readFile "../test_short.csv"
+
+    let parseFile = parseCsv where
         parseCsv = parse csvFile "unknown" 
-        csvFile = sepBy line (char '\n')
+        csvFile = sepBy line (string "\r\n")
         line = sepBy cell (char ',')
         cell =  many (noneOf "\n\r,")
 
+    let csvTrainingSet = parseFile trainFile
+    let csvTestSet = parseFile testFile
+
     case csvTrainingSet of 
-        Left e -> do putStrLn "Error parsing CSV"
-                     print e
-        Right r -> do print $ map toLetter r
-        --Right r -> do print r
+        Left trainError  -> do putStrLn "Error parsing Training CSV"
+                               print trainError
+        Right train -> case csvTestSet of 
+            Left testError -> do putStrLn "Error parsing Test CSV"
+                                 print testError
+            Right test -> do let distancemetric = minkowskiDistance 2
+                             let testSet = [toLetter x | x <- test, x /= [""]]
+                             let trainSet = [toLetter x | x <- train, x /= [""]]
+                             let tieBreaker = (\l -> feature l !! 1)
+                             let results = [value test == vote tieBreaker (kNearestNeighbor distancemetric trainSet test 5) | test <- testSet]
+                             let correctResults = length (filter (\x -> x == True) results)
+                             let totalResults = length results
+                             let fractionCorrect = (fromIntegral correctResults) / (fromIntegral totalResults)
+                             print $ show fractionCorrect
