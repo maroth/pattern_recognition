131e3e417b4ef0de49205307e532337eabdd6f5d
Markus Roth <maroth@gmail.com>
2016-03-13 18:21:39 +0100

    k-means clustering

--- Exercise1/1b/Main.hs
+++ Exercise1/1b/Main.hs
@@ -1,7 +1,7 @@
 module Main where
 
 import Data.Function (on)
-import Data.List (minimumBy, maximumBy, transpose, delete)
+import Data.List (minimumBy, maximumBy, groupBy, transpose, delete)
 import Data.Ord (comparing)
 import Text.ParserCombinators.Parsec
 
@@ -14,24 +14,33 @@ value (Letter val _ ) = val
 feature :: Letter -> [Float]
 feature (Letter _ feat ) = feat
 
+--K-Means Clustering
+cluster :: [Letter] -> [[Float]] -> Int -> [[Letter]]
+cluster letters clusterCenters 0 = makeClusters letters clusterCenters
+cluster letters clusterCenters remainingIterations = cluster letters newClusterCenters (pred remainingIterations) where
+    newClusters = makeClusters letters clusterCenters
+    newClusterCenters = map clusterCenter newClusters
+
 --Minkowski Distance (m=1:manhattan, m=2:euclidean)
 minkowskiDistance :: Float -> [Float] -> [Float] -> Float
 minkowskiDistance m xs ps = (sum $ zipWith (\x p -> abs $ (x - p) ** m) xs ps) ** (1 / m)
 
---K-Means Clustering
+--Find the center of a cluster given by a list of letters
 clusterCenter :: [Letter] -> [Float]
 clusterCenter [] = []
 clusterCenter items = map (normalize . sum) $ transpose $ map feature items where
-                          normalize a = a / fromIntegral (length items)
+    normalize a = a / fromIntegral (length items)
 
-nearestCluster :: [[Letter]] -> Letter -> [Letter] 
+--Find the center of the nearest cluster given a letter and a list of cluster centers
+nearestCluster :: [[Float]] -> Letter -> [Float]
 nearestCluster [] _ = []
 nearestCluster (singleCluster:[]) _ = singleCluster
-nearestCluster clusters letter = fst $ minimumBy (compare `on` snd) [(cluster, distanceTo cluster) | cluster <- clusters] where
-                                     distanceTo cluster = minkowskiDistance 2 (feature letter) (clusterCenter cluster) 
+nearestCluster clusterCenters letter = fst $ minimum [(clusterCenter, distanceTo clusterCenter) | clusterCenter <- clusterCenters] where
+    distanceTo clusterCenter = minkowskiDistance 2 (feature letter) clusterCenter
 
-
-
+--make clusters by assigning each letter to its closest cluster center
+makeClusters :: [Letter] -> [[Float]] -> [[Letter]]
+makeClusters letters clusterCenters = groupBy (\a b -> nearestCluster clusterCenters a == nearestCluster clusterCenters b) letters
 
 --return a list of cluster centers using spanning cluster center selection
 initialClusterCenters :: [Letter] -> Int -> [[Float]]
